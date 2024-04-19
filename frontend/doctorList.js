2a0c2822ba93222fdd299a3714ea97e94a0d255b
tahmidur_saad <tahmidursaad@gmail.com>
2024-04-19 22:49:51 +0600

    make json problem

--- frontend/doctorList.js
+++ frontend/doctorList.js
@@ -1,11 +1,39 @@
 function loadDoctors(){
-    apiLink = '';
-    fetch(apiLink)
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => displayDoctor(data))
+    .then(data1 => displayDoctor(data1))
 }
 
-function displayDoctor(data){
-    
+function loadAllDoctors(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data2 => console.log(data2))
+}
 
+function displayDoctor(data1){
+    const postAllDoctors = document.getElementById('allDoctorsContainer');
+    for(const post of data1){
+        const div = document.createElement('div');
+        div.innerHTML = 
+        `<h4>name - </h4>`;
+        postAllDoctors.appendChild(div);
+
+    }
+}
+
+
+function displayAllDoctor(data2){
+    const postAllDoctors = document.getElementById('allDoctorsContainer');
+    for(const post of data2){
+        const tr = document.createElement('tr');
+        tr.innerHTML = 
+        `
+        <th>${post.id}</th>
+        <td>${post.name}</td>
+        <td>${post.email}</td>
+        <td>Blue</td>
+        `;
+        postAllDoctors.appendChild(tr);
+
+    }
 }
