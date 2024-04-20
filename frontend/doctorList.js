909bf8df6f083b4dad088ae0ff47135b8e480fbc
tahmidur_saad <tahmidursaad@gmail.com>
2024-04-20 12:31:56 +0600

    fixed some api problem

--- frontend/doctorList.js
+++ frontend/doctorList.js
@@ -4,34 +4,36 @@ function loadDoctors(){
     .then(data1 => displayDoctor(data1))
 }
 
-function loadAllDoctors(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data2 => console.log(data2))
-}
-
 function displayDoctor(data1){
-    const postAllDoctors = document.getElementById('allDoctorsContainer');
+    const postSelectedDoctors = document.getElementById('selectedDoctorsContainer');
     for(const post of data1){
-        const div = document.createElement('div');
-        div.innerHTML = 
-        `<h4>name - </h4>`;
-        postAllDoctors.appendChild(div);
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${post.id}</td>
+        <td>${post.name}</td>
+        <td>${post.email}</td>
+        <td>${post.address.city}</td>
+        `;
+        postSelectedDoctors.appendChild(tr);
 
     }
 }
 
+function loadAllDoctors(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data2 =>displayAllDoctor(data2))
+}
 
 function displayAllDoctor(data2){
     const postAllDoctors = document.getElementById('allDoctorsContainer');
     for(const post of data2){
         const tr = document.createElement('tr');
-        tr.innerHTML = 
-        `
-        <th>${post.id}</th>
+        tr.innerHTML = `
+        <td>${post.id}</td>
         <td>${post.name}</td>
         <td>${post.email}</td>
-        <td>Blue</td>
+        <td>${post.address.city}</td>
         `;
         postAllDoctors.appendChild(tr);
 
